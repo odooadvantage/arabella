4e40ecc029aed69d832e17ab5e8387d0979d9e8a
Younis <m.younis.mahsud@gmail.com>
2021-02-10 21:55:11 +0500

    custom header and Invoice custom PDF print

---
+++ header_footer_erpify/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'ERPify - Header Footer',
+    'description': 'ERPify header footer',
+    'author': 'ERPify',
+    'depends': ['web'],
+    'data': ['report_header.xml'],
+    'installable': True,
+}
